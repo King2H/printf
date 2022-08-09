410b025b6d9fb2540d8df163738fa11225ca7f4d
King2H <neg2htt@gmail.com>
2022-08-09 13:18:48 +0000

    Changes to four files

--- int_dec.c
+++ int_dec.c
@@ -1,18 +1,9 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
 * _print_dec - print d
 * @d: integer
 * Return: count
 */
-=======
-
-/**
- * _print_dec - print d
- * @d: integer
- * Return: count
- */
->>>>>>> acfd0f1dfecabc411eaa6af8bac83b93a4ec28eb
 int _print_dec(va_list d)
 {
 	int dig[10];
@@ -42,25 +33,15 @@ int _print_dec(va_list d)
 			_putchar(dig[i] + '0');
 			count++;
 		}
-<<<<<<< HEAD
 
-=======
->>>>>>> acfd0f1dfecabc411eaa6af8bac83b93a4ec28eb
 	}
 	return (count);
 }
 /**
-<<<<<<< HEAD
 * _print_int - print integer
 * @i: integer
 * Return: count
 */
-=======
- * _print_int - print integer
- * @i: integer
- * Return: count
- */
->>>>>>> acfd0f1dfecabc411eaa6af8bac83b93a4ec28eb
 int _print_int(va_list i)
 {
 	int dig[10];
@@ -90,15 +71,9 @@ int _print_int(va_list i)
 			_putchar(dig[j] + '0');
 			count++;
 		}
-<<<<<<< HEAD
 
-=======
->>>>>>> acfd0f1dfecabc411eaa6af8bac83b93a4ec28eb
 	}
 	return (count);
 }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> acfd0f1dfecabc411eaa6af8bac83b93a4ec28eb
